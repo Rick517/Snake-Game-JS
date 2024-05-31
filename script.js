b9d19fd13286f6870502bbc73184442e42bebd77
Dmitry <160389595+Rick517@users.noreply.github.com>
2024-05-31 23:15:11 +0500

    Fixed seen by berries

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function reset() {
 function randomPosition() {
     // NOTE plus one because max(random) < 1
     b1 = Math.floor(Math.random() * i); b2 = Math.floor(Math.random() * i);
-    while (b1 === x && b2 === y || seen[y][x]) { // x and y are initial position != initial berry
+    while (b1 === x && b2 === y || seen[b2][b1]) { // x and y are initial position != initial berry
         b1 = Math.floor(Math.random() * i); b2 = Math.floor(Math.random() * i);
     }
 }
